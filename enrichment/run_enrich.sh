a14060daa1348dfcba7e915126c30b67328d6cc8
gda7090 <axiandan@163.com>
2019-05-05 21:34:27 +0800

    tools

--- enrichment/run_enrich.sh
+++ enrichment/run_enrich.sh
@@ -1,6 +1,6 @@
-python /NJPROJ2/RNA_S/pipeline/Advanced/enrichment/enrich_step1.py \
-	--indir /NJPROJ2/RNA_S/SHOUHOU/P101SC17112677-01-B15-18/shouhou_0629/1.aggr_ZLJ_BM_zlj_pb_2 \
+python enrich_step1.py \
+	--indir . \ 
 	--sampleid ZLJ_BMvszlj_pb_2 \
 	--species GRCh38 \
-	--outdir /NJPROJ2/RNA_S/SHOUHOU/P101SC17112677-01-B15-18/shouhou_0629/1.aggr_ZLJ_BM_zlj_pb_2/Enrichment
+	--outdir . 
 
