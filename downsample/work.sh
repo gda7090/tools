f899b27a58a6bfebe3fce943acf2bfeca8088c2c
lindan <axiandan@163.com>
2019-05-06 10:00:38 +0800

    Update work.sh

--- downsample/work.sh
+++ downsample/work.sh
@@ -1,5 +1,5 @@
-#/NJPROJ2/MICRO/PROJ/yangfenglong/software/miniconda3/lib/R/bin/Rscript downsample.R A_4.specifc.clones.txt downsampled_A_4.specifc.clones.txt 2 46472 2
+#software/miniconda3/lib/R/bin/Rscript downsample.R A_4.specifc.clones.txt downsampled_A_4.specifc.clones.txt 2 46472 2
 le pre_normalize.specific_clone.list |perl -ne 'chomp;$smp=$_;open(IN,$_);<IN>;$sum=0;while(<IN>){$c=(split/\t/,$_)[1];$sum+=$c};print "$smp\t$sum\n"' >sum.list
 sort -k2 -n sum.list #取最小数据量值26480
-le pre_normalize.specific_clone.list |perl -ne 'chomp;$_=~/(02.mix.*)\/(.*)/;chomp($d=`pwd`);print "mkdir -p $d/$1\n cd $d/$1\n /NJPROJ2/MICRO/PROJ/yangfenglong/software/miniconda3/lib/R/bin/Rscript $d/downsample.R $_ $d/$1/$2 2 26480 5\n\n"' >down.sh
-#  perl /NJPROJ2/MICRO/share/MetaGenome_pipeline/MetaGenome_pipeline_V5.1/lib/00.Commbin/super_worker.pl --qalter --cyqt 1 --maxjob 200 --sleept 600 --resource 5G  down.sh  -splits '\n\n' --prefix downs & 
+le pre_normalize.specific_clone.list |perl -ne 'chomp;$_=~/(02.mix.*)\/(.*)/;chomp($d=`pwd`);print "mkdir -p $d/$1\n cd $d/$1\n software/miniconda3/lib/R/bin/Rscript $d/downsample.R $_ $d/$1/$2 2 26480 5\n\n"' >down.sh
+#  perl super_worker.pl --qalter --cyqt 1 --maxjob 200 --sleept 600 --resource 5G  down.sh  -splits '\n\n' --prefix downs & 
