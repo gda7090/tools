2e7547565ceaafc0d1ab0250e792a52577bc14fb
lindan <axiandan@163.com>
2019-05-06 10:02:03 +0800

    Update demo.sh

--- diff_group/demo.sh
+++ diff_group/demo.sh
@@ -1,1 +1,1 @@
-/NJPROJ2/RNA_S/pipeline/Advanced/diff_group/edgeR --count ../CellRanger_aggr/BM_TregvsPB_Treg/BM_TregvsPB_Treg/outs/BM_TregvsPB_Treg_gene_bar.csv --condition condition --cpname BM_TregvsPB_Treg --design normal --fc 1 --padj 0.05 --anno /NJPROJ2/RNA_S/pipeline/Advanced/diff_group/GRCh38.gene.xls --outdir BM_TregvsPB_Treg_diff
+edgeR --count CellRanger_aggr/BM_TregvsPB_Treg/BM_TregvsPB_Treg/outs/BM_TregvsPB_Treg_gene_bar.csv --condition condition --cpname BM_TregvsPB_Treg --design normal --fc 1 --padj 0.05 --anno GRCh38.gene.xls --outdir BM_TregvsPB_Treg_diff
